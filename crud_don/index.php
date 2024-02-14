39784de3e651c9dd62c338ca9624cba4d0ccf9f6
BurapaThongOn <136049415+BurapaThongOn@users.noreply.github.com>
2024-02-13 20:07:33 -0800

    Add files via upload

---
+++ crud_don/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php require_once "connect.php" ?>
+    <?php require_once "setHead.php" ?>
+</head>
+<body>
+    
+</body>
+    <h1 class="container mt-5"> นักเรียนตั้งใจเรียน</h1>
+<?php require_once "setFoot.php" ?>
+</html>
